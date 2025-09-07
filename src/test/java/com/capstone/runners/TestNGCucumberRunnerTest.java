c5a93204df4d2141cd07d4cbf0202e623fec93a7
Yandamuri Akshay <88223556+YandamuriAkshay@users.noreply.github.com>
2025-09-08 00:33:20 +0530

    Update TestNGCucumberRunnerTest.java

--- src/test/java/com/capstone/runners/TestNGCucumberRunnerTest.java
+++ src/test/java/com/capstone/runners/TestNGCucumberRunnerTest.java
@@ -4,16 +4,16 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-	    features = "src/test/resources/features",
-	    glue = {"com.capstone.stepdefinitions"},
-	    plugin = {
-	        "pretty",
-	        "json:target/cucumber.json", 
-	        "html:target/cucumber-reports.html",
-	        "junit:target/cucumber.xml"
-	    },
-	    monochrome = true
-	)
-
-public class TestNGCucumberRunnerTest extends AbstractTestNGCucumberTests {
+    features = "src/test/resources/features",
+    glue = "com.example.stepdefinitions", 
+    plugin = {
+        "pretty",
+        "html:target/cucumber-reports/cucumber.html",
+        "json:target/cucumber-reports/cucumber.json"
+		"junit:target/cucumber-reports/cucumber.xml"
+    },
+    monochrome = true
+)
+public class TestNGRunner extends AbstractTestNGCucumberTests {
+}
 }
